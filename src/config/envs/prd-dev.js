77037795a7ecd8e9a2120b50303131d00a734e65
mf1994 <18664743505@163.com>
2022-04-19 10:15:31 +0800

    refactor: db-check

---
+++ src/config/envs/prd-dev.js
@@ -0,0 +1,25 @@
+/**
+ * @description 配置
+ */
+
+const devConf = require('./dev')
+
+// 修改 redis 连接配置
+Object.assign(devConf.redisConf, {
+    // 和 docker-compose 中配置的 service 名字一致
+    // 【注意】端口依然是 6379 ，而不是 6378 ，后者是宿主机的端口
+    host: 'editor-redis',
+})
+
+// 修改 mongodb 连接配置
+Object.assign(devConf.mongodbConf, {
+    host: 'editor-mongo', // 和 docker-compose 中配置的 service 名字一致
+})
+
+// 修改 mysql 连接配置
+Object.assign(devConf.mysqlConf, {
+    host: 'editor-mysql', // 和 docker-compose 中配置的 service 名字一致
+})
+
+
+module.exports = devConf
--- src/routes/index.js
+++ src/routes/index.js
@@ -7,16 +7,16 @@ const {cacheSet, cacheGet} = require('../cache/index')
 
 // 测试数据库连接
 router.get('/api/db-check', async ctx => {
-    // // 测试mysql连接
-    // const mysqlRes = await testMysqlConn()
-    // // 测试mongodb连接
-    // let mongodbConn
-    // try {
-    //     mongodbConn = true
-    //     await WorkModel.findOne()
-    // } catch (ex) {
-    //     mongodbConn = false
-    // }
+    // 测试mysql连接
+    const mysqlRes = await testMysqlConn()
+    // 测试mongodb连接
+    let mongodbConn
+    try {
+        mongodbConn = true
+        await WorkModel.findOne()
+    } catch (ex) {
+        mongodbConn = false
+    }
     // 测试redis连接
     cacheSet('name', 'biz editor server OK - by redis')
     const redisTestVal = await cacheGet('name')
@@ -26,8 +26,8 @@ router.get('/api/db-check', async ctx => {
             name: 'biz editor serve',
             version: packageInfo.version,
             ENV,
-            // mysqlConn: mysqlRes.length > 0,
-            // mongodbConn,
+            mysqlConn: mysqlRes.length > 0,
+            mongodbConn,
             redisConn: redisTestVal != null
         }
     }
