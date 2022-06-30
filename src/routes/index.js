bebaa8c3eb5a6e68c695faf3582d2d927d019417
梅凡 <meifan@meifandeMacBook-Pro.local>
2022-06-30 09:13:51 +0800

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,34 +1,34 @@
 const router = require('koa-router')()
 const {ENV} = require('../utils/env')
 const packageInfo = require('../../package.json')
-const testMysqlConn = require('../db/mysql2')
+// const testMysqlConn = require('../db/mysql2')
 const {WorkModel} = require("../models/WorkModel")
 const {cacheSet, cacheGet} = require('../cache/index')
 
 // 测试数据库连接
 router.get('/api/db-check', async ctx => {
     // 测试mysql连接
-    const mysqlRes = await testMysqlConn()
+    // const mysqlRes = await testMysqlConn()
     // 测试mongodb连接
-    let mongodbConn
-    try {
-        mongodbConn = true
-        await WorkModel.findOne()
-    } catch (ex) {
-        mongodbConn = false
-    }
+    // let mongodbConn
+    // try {
+    //     mongodbConn = true
+    //     await WorkModel.findOne()
+    // } catch (ex) {
+    //     mongodbConn = false
+    // }
     // 测试redis连接
-    cacheSet('name', 'biz editor server OK - by redis')
-    const redisTestVal = await cacheGet('name')
+    // cacheSet('name', 'biz editor server OK - by redis')
+    // const redisTestVal = await cacheGet('name')
     ctx.body = {
         errno: 0,
         data: {
             name: 'biz editor serve',
             version: packageInfo.version,
             ENV,
-            mysqlConn: mysqlRes.length > 0,
-            mongodbConn,
-            redisConn: redisTestVal != null
+            // mysqlConn: mysqlRes.length > 0,
+            // mongodbConn,
+            // redisConn: redisTestVal != null
         }
     }
 })
