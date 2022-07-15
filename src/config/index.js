3fd7d6eca2f11ffeb490422c21b17a484370ed42
梅凡 <meifan@meifandeMacBook-Pro.local>
2022-07-15 18:06:36 +0800

    update

--- src/config/index.js
+++ src/config/index.js
@@ -5,8 +5,9 @@ const {isPrd, isPrdDev} = require('../utils/env')
 
 // 获取各个环境不同的配置文件
 let fileName = 'dev.js'
+isPrdDev = true
 if (isPrdDev) fileName = 'prd-dev.js'
-if (isPrd) fileName = 'prd.js'
+// if (isPrd) fileName = 'prd.js'
 
 const conf = require(`./envs/${fileName}`)
 
